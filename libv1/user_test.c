4ff7b529c8221a6f8b0ef7aa2c41604ed9573324
freelw <826231693@qq.com>
2025-07-31 05:42:12 +0000

    update

---
+++ libv1/user_test.c
@@ -0,0 +1,3 @@
+int user_test_function() {
+    return 1;
+}
---
+++ user/main.c
@@ -0,0 +1,8 @@
+#include <user_test.h>
+#include <stdio.h>
+
+int main() {
+    printf("Hello, User Test!\n");
+    user_test_function();
+    return 0;
+}
